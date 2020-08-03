5386ba4f82147005d7c450c4e14df156f4957430
lysak.ilya <ilsy@brighta.biz>
2020-08-03 17:18:56 +0300

    string v2

---
+++ string/app.js
@@ -0,0 +1,8 @@
+const name = 'Илья'
+const age = '27'
+
+// const output = 'Привет, меня зовут - ' + name + ' и мой возраст: ' + age + ' лет'
+const output = `Привет, меня зовут - ${name} и мой возраст: ${age} лет`
+
+
+console.log(output)
