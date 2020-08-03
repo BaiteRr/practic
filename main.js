3d472806f2c5bcb76419a762b1ee261a3430e031
lysak.ilya <ilsy@brighta.biz>
2020-08-03 14:35:26 +0300

    next

--- main.js
+++ main.js
@@ -4,8 +4,7 @@ const training = document.querySelector('#training')
 training.textContent = 'Ку Ку'
 training.style.textAlign = 'center'
 training.style.backgroundColor = 'black'
-training.style.padding = '2em'
+training.style.margin = '2em'
 training.style.color = 'red'
 
-header.style.color = 'red'
 
