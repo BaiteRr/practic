b82742a76f3e6dd4476b5d357c7098fb17f19b6a
lysak.ilya <ilsy@brighta.biz>
2020-08-03 14:04:04 +0300

    1

---
+++ main.js
@@ -0,0 +1,10 @@
+const header = document.querySelector('.hello')
+const training = document.querySelector('#training')
+
+training.textContent = 'Ку Ку'
+training.style.textAlign = 'center'
+training.style.backgroundColor = 'black'
+training.style.margin = '2em'
+
+header.style.color = 'red'
+
