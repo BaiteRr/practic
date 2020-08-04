8e9c475323412f8d1a872e1a8384b6ee557e3ac9
lysak.ilya <ilsy@brighta.biz>
2020-08-04 18:23:43 +0300

    TS

---
+++ TypeScript/index.js
@@ -0,0 +1,9 @@
+// // tsc index.ts
+var User = /** @class */ (function () {
+    function User(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    return User;
+}());
++' Hello';
