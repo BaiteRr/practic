221a71f226c6b562f18d38fb890d4318ff3bf2a2
lysak.ilya <ilsy@brighta.biz>
2020-08-05 18:24:21 +0300

    first install

--- ANGULAR/README.md
+++ ANGULAR/README.md
@@ -1,4 +1,4 @@
-# AngularNew
+# ANGULAR
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.5.
 
--- ANGULAR/e2e/src/app.e2e-spec.ts
+++ ANGULAR/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('AngularNew app is running!');
+    expect(page.getTitleText()).toEqual('ANGULAR app is running!');
   });
 
   afterEach(async () => {
