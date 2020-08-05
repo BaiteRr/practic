4857c7eef5515bd298e22fd60c86ab3d855e2433
lysak.ilya <ilsy@brighta.biz>
2020-08-05 03:36:24 +0300

    next

--- Angular/AngularNew/README.md
+++ Angular/AngularNew/README.md
@@ -1,4 +1,4 @@
-# AngularLearn
+# AngularNew
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.5.
 
--- Angular/AngularNew/src/app/app.component.spec.ts
+++ Angular/AngularNew/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Angular-learn'`, () => {
+  it(`should have as title 'AngularNew'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular-learn');
+    expect(app.title).toEqual('AngularNew');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Angular-learn app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('AngularNew app is running!');
   });
 });
